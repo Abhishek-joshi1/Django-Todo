420ba8eccbd7d852d3f3fe7f433afd19c564ee38
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-07-15 23:04:53 -0700

    Successfully created add task section

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from todos.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(isCompleted = False)
-    print(tasks)
+    tasks = Task.objects.filter(isCompleted = False).order_by('-updatedAt')
     context = {
         "tasks" : tasks,
     }
