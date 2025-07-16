ffcc5889c853942aaf73cdbac29ff623731638db
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-07-16 05:00:10 -0700

    add all functionalitied to project

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
     path('markAsDone/<int:pk>/', views.markAsDone, name='markAsDone'),
+    path('markAsUnDone/<int:pk>/', views.markAsUnDone, name='markAsUnDone'),
+    path('Edit_Task/<int:pk>/', views.Edit_Task, name='Edit_Task'),
+    path('Delete_Task/<int:pk>/', views.Delete_Task, name='Delete_Task'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -14,3 +14,28 @@ def markAsDone(request, pk):
     task.isCompleted = True
     task.save()
     return redirect('home')
+
+def markAsUnDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.isCompleted = False
+    task.save()
+    return redirect('home') 
+
+def Edit_Task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['tasks']
+        get_task.task = new_task
+        print(get_task)
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
+
+def Delete_Task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
