968d5d5b55185464998e9c263fe43df9cfd87fea
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-07-15 20:18:19 -0700

    Create todos app

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todos.models import Task
 
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(isCompleted = False)
+    print(tasks)
+    context = {
+        "tasks" : tasks,
+    }
+    return render(request, 'home.html', context)
